721ead1c55e57da29ec4042fd26f2942f96aef2e
Christophe Adriaensens <christophe.adriaensens@gmail.com>
2020-12-01 16:29:49 +0100

    First working version without styling

---
+++ script.js
@@ -0,0 +1,45 @@
+const citySearch = document.querySelector(".city");
+const cityLocation = document.querySelector(".location");
+const cityTemperatureDayZero = document.querySelector(".temperatureDayZero");
+const cityTemperatureDayOne = document.querySelector(".temperatureDayOne");
+const cityTemperatureDayTwo = document.querySelector(".temperatureDayTwo");
+const cityTemperatureDayThree = document.querySelector(".temperatureDayThree");
+const cityTemperatureDayFour = document.querySelector(".temperatureDayFour");
+
+let daysCount = 5;
+
+
+
+citySearch.addEventListener("keyup", function (event){
+    if (event.key == "Enter") {
+        event.preventDefault();
+        document.querySelector("#run").click();
+    }
+})
+
+document.querySelector("#run").addEventListener("click", function() {
+    console.log(citySearch.value);
+    fetch (`https://api.weatherbit.io/v2.0/forecast/daily?city=${citySearch.value}&key=5c845431685a49938d7db2daf29433fd&days=${daysCount}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            let selectedCity = data["city_name"];
+
+
+            let tempDayZero = data["data"]["0"]["temp"];
+            let tempDayOne = data["data"]["1"]["temp"];
+            let tempDayTwo = data["data"]["2"]["temp"];
+            let tempDayThree = data["data"]["3"]["temp"];
+            let tempDayFour = data["data"]["4"]["temp"];
+
+
+            cityLocation.innerHTML = `Current weather forecast for ${selectedCity}`;
+            cityTemperatureDayZero.innerHTML = `The temperature is ${tempDayZero}°C.`;
+            cityTemperatureDayOne.innerHTML = `The temperature for tomorrow is ${tempDayOne}°C.`;
+            cityTemperatureDayTwo.innerHTML = `The temperature for the day after is ${tempDayTwo}°C.`;
+            cityTemperatureDayThree.innerHTML = `The temperature for the day after is ${tempDayThree}°C.`;
+            cityTemperatureDayFour.innerHTML = `The temperature for the day after is ${tempDayFour}°C.`;
+        })
+
+        .catch(err => alert("Wrong city name."))
+})
